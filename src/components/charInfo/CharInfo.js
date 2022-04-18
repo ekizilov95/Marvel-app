fb5301050f4aded7ec0a368c42a01c19a413c6e3
Evgen <e.kizilov95@gmail.com>
2022-04-18 23:31:43 +0300

    fix

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -42,31 +42,31 @@ const Content = ({char}) => {
     return (
         <>
             <div className="char__basics">
-                <img src={ char.image } alt={ char.name }/>
+                <img src={char.image} alt={ char.name }/>
                 <div>
                     <div className="char__info-name">{ char.name }</div>
                     <div className="char__btns">
-                        <a href={ char.homepage } className="button button__main">
+                        <a href={char.homepage} className="button button__main">
                             <div className="inner">homepage</div>
                         </a>
-                        <a href={ char.wiki } className="button button__secondary">
+                        <a href={char.wiki} className="button button__secondary">
                             <div className="inner">Wiki</div>
                         </a>
                     </div>
                 </div>
             </div>
             <div className="char__descr">
-                { char.description } 
+                {char.description} 
             </div>
             
-            <div className="char__comics" style={ char.stories.length < 1 ? {display: 'none'} : null}>Comics:</div>
+            <div className="char__comics" style={char.stories.length < 1 ? {display: 'none'} : null}>Comics:</div>
             <ul className="char__comics-list">
                 {
                     char.stories.map((item, i) => {
                         if(i < 10)  {
                             return (
                                 <li className="char__comics-item" key={i}>
-                                    { item.name }
+                                    {item.name}
                                 </li>
                             )
                         }
